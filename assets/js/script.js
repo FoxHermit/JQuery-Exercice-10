9ab47db681b98fc7631a7ab9703429e1c47c6d3c
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-11 17:00:44 +0200

    Corrigé final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // Au passage de la souris (évènement mouseover) sur mon élément qui a l'ID "green" "red" ou "blue", je modifie le CSS de mon élément ID "text".
 $(function(){
-  $('#green').mouseover(function(){
+  $('.color').mouseover(function(){
+    buttoncolor = $(this).attr('id'); // Je stocke dans la variable "buttoncolor" la valeur de l'attribut ID de l'élément sur lequel je suis.
+    console.log(buttoncolor);
+    $('#text').css('color', buttoncolor);
+  });
+
+/*  On peut faire comme ça aussi, mais c'est plus long :
+$('#green').mouseover(function(){
     $('#text').css('color', 'green');
   });
   $('#red').mouseover(function(){
@@ -8,8 +15,9 @@ $(function(){
   });
   $('#blue').mouseover(function(){
     $('#text').css('color', 'blue');
-  });
-// Je cible tous les éléments qui ont la class "color", au moment où ma souris quitte ces éléments, je modifie le CSS de mon élément ID "text".  
+  }); */
+
+// Je cible tous les éléments qui ont la class "color", au moment où ma souris quitte ces éléments, je modifie le CSS de mon élément ID "text".
   $('.color').mouseout(function(){
     $('#text').css('color', 'black');
   });
